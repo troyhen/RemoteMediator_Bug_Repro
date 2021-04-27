20ba0359db2579880e8e4a88ccb06e11f4ce0747
Troy <troyhen@churchofjesuschrist.org>
2021-04-26 18:09:59 -0600

    Clarify names and add commenes

--- app/src/main/java/com/troy/remotemediatorbugrepro/MainActivity.kt
+++ app/src/main/java/com/troy/remotemediatorbugrepro/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
-import androidx.paging.DifferCallback
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
@@ -22,7 +21,6 @@ import androidx.paging.cachedIn
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 
@@ -30,11 +28,11 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
 
-    private lateinit var cachedAdapter: ContentAdapter
-    private lateinit var directAdapter: ContentAdapter
-    private lateinit var direct: ToggleButton
-    private lateinit var cached: ToggleButton
-    private lateinit var instructions: TextView
+    private lateinit var sourceAdapter: ContentAdapter
+    private lateinit var mediatorAdapter: ContentAdapter
+    private lateinit var source: ToggleButton
+    private lateinit var mediator: ToggleButton
+    private lateinit var info: TextView
     private lateinit var recycler: RecyclerView
 
     private var job: Job? = null
@@ -42,43 +40,43 @@ class MainActivity : AppCompatActivity() {
     private var mode: Mode = Mode.HOME
         set(value) {
             field = value
-            instructions.isVisible = value == Mode.HOME
+            info.isVisible = value == Mode.HOME
             recycler.isVisible = value != Mode.HOME
-            direct.isChecked = value == Mode.DIRECT
-            cached.isChecked = value == Mode.CACHED
+            source.isChecked = value == Mode.SOURCE
+            mediator.isChecked = value == Mode.MEDIATOR
             update()
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        direct = findViewById(R.id.direct)
-        cached = findViewById(R.id.cached)
+        source = findViewById(R.id.direct)
+        mediator = findViewById(R.id.cached)
         recycler = findViewById(R.id.recycler)
         recycler.layoutManager = LinearLayoutManager(this)
-        cachedAdapter = ContentAdapter()
-        directAdapter = ContentAdapter()
-        instructions = findViewById(R.id.instructions)
-        direct.setOnClickListener { mode = Mode.DIRECT }
-        cached.setOnClickListener { mode = Mode.CACHED }
+        mediatorAdapter = ContentAdapter()
+        sourceAdapter = ContentAdapter()
+        info = findViewById(R.id.info)
+        source.setOnClickListener { mode = Mode.SOURCE }
+        mediator.setOnClickListener { mode = Mode.MEDIATOR }
     }
 
     private fun update() {
         job?.cancel()
         job = lifecycleScope.launchWhenStarted {
             when (mode) {
-                Mode.CACHED -> {
-                    val flow = viewModel.cachedPagingDataFlow
-                    val adapter = cachedAdapter
+                Mode.MEDIATOR -> {
+                    val flow = viewModel.mediatorPagingDataFlow
+                    val adapter = mediatorAdapter
                     recycler.adapter = adapter
                     adapter.refresh()
                     flow.collect {
                         adapter.submitData(it)
                     }
                 }
-                Mode.DIRECT -> {
-                    val flow = viewModel.directPagingDataFlow
-                    val adapter = directAdapter
+                Mode.SOURCE -> {
+                    val flow = viewModel.sourcePagingDataFlow
+                    val adapter = sourceAdapter
                     recycler.adapter = adapter
                     adapter.refresh()
                     flow.collect {
@@ -90,7 +88,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     enum class Mode {
-        HOME, DIRECT, CACHED,
+        HOME, SOURCE, MEDIATOR,
     }
 }
 
@@ -123,12 +121,22 @@ class MainViewModel : ViewModel() {
 
     private val db = App.app.db
 
-    val directPagingDataFlow = Pager(PagingConfig(pageSize = 5, initialLoadSize = 5, enablePlaceholders = false)) {
+    val sourcePagingDataFlow = Pager(
+        config = PagingConfig(pageSize = PAGE_SIZE, initialLoadSize = PAGE_SIZE, enablePlaceholders = false)
+    ) {
         ContentSource(App.app.backend)
     }.flow.cachedIn(viewModelScope)
 
     @OptIn(ExperimentalPagingApi::class)
-    val cachedPagingDataFlow = Pager(PagingConfig(pageSize = 5, initialLoadSize = 5, enablePlaceholders = false), remoteMediator = ContentMediator(App.app.db, App.app.backend)) {
+    val mediatorPagingDataFlow = Pager(
+        config = PagingConfig(pageSize = PAGE_SIZE, initialLoadSize = PAGE_SIZE, enablePlaceholders = false),
+        remoteMediator = ContentMediator(App.app.db, App.app.backend)
+    ) {
         db.contentDao.contentSource()
     }.flow
+
+    companion object {
+        // We use a small page size to more easily show the page loading problems with RemoteMediator
+        private const val PAGE_SIZE = 5
+    }
 }
